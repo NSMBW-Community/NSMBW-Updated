183af2fc2bd9acf9da9a74b8cd8d1e32a675ee70
RoadrunnerWMC <RoadrunnerWMC@users.noreply.github.com>
2022-10-02 18:22:21 -0400

    Seems I forgot to include this in some previous commit

--- nsmbw_constants.py
+++ nsmbw_constants.py
@@ -1,6 +1,5 @@
 REGIONS = ['P', 'E', 'J', 'K', 'W', 'C']
 VERSIONS = ['P1', 'E1', 'J1', 'P2', 'E2', 'J2', 'P3', 'J3', 'K', 'W', 'C']
-DEFAULT_VERSION = 'P1'
 LANG_FOLDER_NAMES = {
     'P1': 'EU',
     'E1': 'US',
